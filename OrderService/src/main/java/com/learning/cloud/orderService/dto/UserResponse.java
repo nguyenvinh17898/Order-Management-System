418a2dba852b862cffe2baa6ad8a4e33d93080b7
nguyenvinh17898 <toiyeudanang1998@gmail.com>
2025-10-10 21:25:01 +0700

    Initial commit

---
+++ OrderService/src/main/java/com/learning/cloud/orderService/dto/UserResponse.java
@@ -0,0 +1,51 @@
+package com.learning.cloud.userservice.dto;
+
+public class UserResponse {
+
+    public UserResponse(long id, String name, String email, String username) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+    }
+
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String username;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ OrderService/src/main/java/com/learning/cloud/orderService/util/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.learning.cloud.orderservice.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+}
